6c6238ed07d56e9a9a7e1195ac1db904f46e32f6
y_okada <yasokada@gmail.com>
2015-08-27 18:55:09 +0900

    add ScreenSizeKeeper to keep the size of the screen for android

---
+++ Assets/ScreenSizeKeeper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenSizeKeeper : MonoBehaviour {
+
+	public Button myButton; // set UI>Button whose width is used as standard
+	
+	void Start () {
+		float aspect = (float)Screen.height / (float)Screen.width;
+		float buttonRatio = 0.5f; // 50%
+		int buttonWidth = (int)myButton.GetComponent<RectTransform> ().rect.width;
+		float newWidth, newHeight;
+		
+		newWidth = buttonWidth / buttonRatio;
+		newHeight = newWidth * aspect;
+		
+		Screen.SetResolution ((int)newWidth, (int)newHeight, false);
+	}   
+
+}
